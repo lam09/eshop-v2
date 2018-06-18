1639caa63fbb22792634415c26d759b083fba855
Anh Lam Tuan <anh.lamtuan@agsoft.sk>
2018-06-18 16:07:36 +0200

    simple pagination

--- src/main/java/com/lam/eshopv2/controller/ProductController.java
+++ src/main/java/com/lam/eshopv2/controller/ProductController.java
@@ -7,15 +7,20 @@ package com.lam.eshopv2.controller;
 import com.lam.eshopv2.entity.Product;
 import com.lam.eshopv2.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 @Transactional
@@ -24,6 +29,7 @@ public class ProductController {
 
     @Autowired
     ProductRepository productRepository;
+
 
     @RequestMapping(value = {"/product/{id}"},method = RequestMethod.GET)
     public String product(@PathVariable Integer id, Model model){
@@ -35,17 +41,30 @@ public class ProductController {
 
     @RequestMapping(value = "/admin/createProduct",method = RequestMethod.GET)
     public String createProduct(Model model){
-        return "addproduct";
+        return "admin/addproduct";
     }
+
     @RequestMapping("/admin/editProduct/{id}")
     public String eidtProduct( @PathVariable Integer id,Model model){
         model.addAttribute("product", productRepository.findProductById(id));
         return "admin/editproduct";
     }
     @RequestMapping(value = "/products",method = RequestMethod.GET)
-    public String productsList(Model model){
+    public String productsList(Model model, //
+                               @RequestParam(value = "page", defaultValue = "1") int page){
+
+        /*pagination style 1 */
+
+
+
         System.out.print("Hello all product");
-        model.addAttribute("products", productRepository.findAll());
+        final int product_count=productRepository.countProducts();
+        final int max_cout_of_page = 5;
+        List<Product> products = productRepository.paginationProducṭ̣(max_cout_of_page,max_cout_of_page*(page-1));
+        model.addAttribute("maxPage", (int)Math.round(product_count/max_cout_of_page));
+        model.addAttribute("currentPage", page);
+        model.addAttribute("products", products);
+        System.out.print("Product count " + products.size() + " max" + product_count);
         return "products";
     }
 
--- src/main/java/com/lam/eshopv2/repository/ProductRepository.java
+++ src/main/java/com/lam/eshopv2/repository/ProductRepository.java
@@ -1,12 +1,31 @@
 package com.lam.eshopv2.repository;
 
 import com.lam.eshopv2.entity.Product;
+import com.lam.eshopv2.pagination.PaginationResult;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+
+
+import java.util.List;
 
 /**
  * Created by a.lam.tuan on 11. 6. 2018.
  */
-public interface ProductRepository extends JpaRepository<Product, Integer>,ProductRepositoryCustom {
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer>,ProductRepositoryCustom,PagingAndSortingRepository<Product, Integer>{
 
     Product findProductById(Integer id);
+
+    @Query(value="select count(*) FROM products ",nativeQuery = true)
+    Integer countProducts();
+
+    @Query(value="select * FROM products " +
+            "limit :LIMIT " +
+            "offset :OFFSET",nativeQuery = true)
+    List<Product> paginationProducṭ̣(@Param("LIMIT") Integer limit,@Param("OFFSET") Integer offset);
+
 }
